3df70719dd14e9e188a755e2755019c7d859d51a
amirf2020 <arminghafouri8869.8869@gmail.com>
2017-05-04 18:18:41 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -1,8 +1,8 @@
 package.path = package.path..';.luarocks/share/lua/5.2/?.lua;.luarocks/share/lua/5.2/?/init.lua'
 package.cpath = package.cpath..';.luarocks/lib/lua/5.2/?.so'
-    bot_token = "Token" --Put You Token Here
+    bot_token = "310918779:AAHWq5UykSafFNkHDI1iXBHIYGePI5nMTd0" --Put You Token Here
    send_api = "https://api.telegram.org/bot"..bot_token
-sudo_id = 157059515
+sudo_id = 317355943
 require('./bot/methods')
 http = require('socket.http')
 https = require('ssl.https')
